17b18440370ed3c425eef35d218cda91dcb6632a
Tiago Matos <tiago@tiagomatos.com>
2020-09-27 09:46:50 +1000

    Add store user endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserStoreRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class UserController extends Controller
 {
+    /**
+     * @return AnonymousResourceCollection
+     */
     public function index()
     {
         $users = User::all();
         return UserResource::collection($users);
+    }
+
+    /**
+     * @param UserStoreRequest $request
+     * @return UserResource
+     */
+    public function store(UserStoreRequest $request)
+    {
+        $input = $request->validated();
+        $user = User::create($input);
+
+        return new UserResource($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function() {
     Route::prefix('users')->group(function() {
         Route::get('', [UserController::class, 'index']);
+        Route::post('', [UserController::class, 'store']);
     });
 });
