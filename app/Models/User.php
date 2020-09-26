99c0f7c54f783f1821029fb23cf5c929841a9e3a
Tiago Matos <tiago@tiagomatos.com>
2020-09-27 09:35:41 +1000

    Add user seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,25 +19,5 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
