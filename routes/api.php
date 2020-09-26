b62e53f341b3f21e93064216b92e443dc4d9e2ff
Tiago Matos <tiago@tiagomatos.com>
2020-09-27 09:39:32 +1000

    Add get all users endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+    Route::prefix('users')->group(function() {
+        Route::get('', [UserController::class, 'index']);
+    });
 });
